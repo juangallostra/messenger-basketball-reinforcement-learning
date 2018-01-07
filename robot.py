dd41c582d99e5d59ddc3daff6cb5b809aa5d2ed3
juangallostra <juangallostra@gmail.com>
2018-01-07 15:45:27 +0100

    Add main funcion

--- robot.py
+++ robot.py
@@ -1,3 +1,21 @@
 # Main module or, in other words, brain of the robot
 import cv
 import ik
+import mappings as m
+import time
+
+def main():
+	processor = cv.module_init(screen_view = False)
+	serial_com = ik.module_init()
+	time.sleep(5)
+	print "Everything initialised"
+	# First iteration
+	ball_coords, basket_coords, score = processor.next()
+	action = m.GRID_TO_ACTIONS[ball_coords[0]]
+	position = m.GRID_TO_COORDINATES[ball_coords[0]]
+	while True:
+		ik.send_position(serial_com, position)
+		ik.send_action(serial_com, action)
+
+if __name__ == '__main__':
+	main()
